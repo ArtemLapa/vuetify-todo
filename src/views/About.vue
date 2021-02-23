24df4a45b362ef3e379b4a1b5d7d1419a74f25ec
ArtemLapa <artyom.lapa@gmail.com>
2021-02-23 18:07:30 +0200

    :sparkles: ready todo list

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus culpa, veniam consequatur quasi sunt iure accusamus ad nostrum doloremque. Sunt, iure ipsum ratione repellat veniam dolorem deleniti vero dolore modi.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus culpa, veniam consequatur quasi sunt iure accusamus ad nostrum doloremque. Sunt, iure ipsum ratione repellat veniam dolorem deleniti vero dolore modi.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus culpa, veniam consequatur quasi sunt iure accusamus ad nostrum doloremque. Sunt, iure ipsum ratione repellat veniam dolorem deleniti vero dolore modi.
+    </p>
   </div>
 </template>
